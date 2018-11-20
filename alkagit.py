6e04d6de4b9089458e02765ff63baedec4811089
jiho <ssy05468@naver.com>
2018-11-20 11:57:48 +0900

    abstract class

--- alkagit.py
+++ alkagit.py
@@ -1,12 +1,7 @@
 class gameBoard:
-    def __init__(self,width,height):
-        self.width=width
-        self.height=height
     pass
 
 class stone:
-    def __init__(self,radius):
-        self.radius=radius
     pass
 
 class gravity:
--- board.py
+++ board.py
@@ -1,33 +1,3 @@
 import pygame
-import TmpFiles.alkagit
+import alkagit
 
-White = (255,255,255)
-pad_width = 700
-pad_height = 700
-
-
-def runGame():
-    global gamepad, clock
-
-    crashed = False
-    while not crashed:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                crashed = True
-
-        gamepad.fill(White)
-        pygame.display.update()
-        clock.tick(60)
-    pygame.quit()
-
-def initGame():
-    global gamepad, clock
-
-    pygame.init()
-    gamepad = pygame.display.set_mode((pad_width, pad_height))
-    pygame.display.set_caption('alkicks')
-
-    clock = pygame.time.Clock()
-    runGame()
-
-initGame()
