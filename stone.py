a56d9dd25edcb4bbfcd5b180ecef2c27a09470da
ssy05468 <36706193+ssy05468@users.noreply.github.com>
2018-11-27 11:38:26 +0900

    Update and rename newstone.py to stone.py

---
+++ stone.py
@@ -0,0 +1,13 @@
+import pygame
+
+class stone:
+
+    def __init__(self,start_x,start_y,radius,mass,level,screen):
+        self.radius=radius
+        self.x=start_x
+        self.y=start_y
+        self.mass=mass
+        self.level=level
+        self.color=(0,0,0)
+        self.isalive=True
+        self.angle=0
