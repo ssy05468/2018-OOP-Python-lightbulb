484692b16e3ac098c6992d8b37ca9f1f1d86d615
ssy05468 <36706193+ssy05468@users.noreply.github.com>
2018-11-27 11:36:49 +0900

    Update alkagit.py

--- alkagit.py
+++ alkagit.py
@@ -1,9 +1,13 @@
 import pygame
-#class gameBoard:
-#    pass
+import container
+import stone
 
-#class stone:
-#    pass
 
-#class gravity:
-#    pass
+window=container.screen('alkagit',700,700,(255,255,255))
+screen=window.screen
+pygame.display.flip()
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
