026c39d89c6e859db81052c3746a539934634230
ssy05468 <36706193+ssy05468@users.noreply.github.com>
2018-11-29 17:43:47 +0900

    주석 추가 및 메인에서 보드 생성

--- finale/realmain.py
+++ finale/realmain.py
@@ -13,26 +13,27 @@ pygame.init()
 # 돌의 개수
 num_of_stone = 10
 
-stone_particles = [Particle_of_Stone(start_x=(i-(i//5)*5)*150+90,start_y=(i//5)*580+10, team=i//5) for i in range(num_of_stone)]
+stone_particles = [Particle_of_Stone(start_x=(i-(i//5)*5)*150+90,start_y=(i//5)*580+10, team=i//5,surface=surface) for i in range(num_of_stone)] #서피스(게임판)전달
 num_particles = num_particles_orig
 particles = [Particle(state=0) for i in range(num_particles)] #중력장을 위한 요소
 
 #화면 생성
-screen=screen('lightbulb',800,600,(255,255,255)) #게임화면
+screen=screen('lightbulb',800,600,(0,0,0)) #게임화면
 window=screen.screen
 surface=pygame.Surface((500,500)) #게임판
 surface.fill((205,154,91)) #바둑판 색
 window.blit(surface, (150, 50)) #바둑판 위치
 
 
-def new_draw():
+def new_draw(): #돌 클래스에서 게임판을 전달받았으므로 draw에서 surface안써줘도됨
+    window.fill((0,0,0))
+    window.blit(surface, (150, 50))
+    for p in particles:
+         p.draw(surface)
+         print(1)
 
-    for p in particles :
-        p.draw(surface)
-        print(1)
-
-    for q in stone_particles :
-        q.draw(surface)
+    for q in stone_particles:
+        q.draw()
 
     pygame.display.flip()
 
