ce19f3ffcd1c78b58a2dc8c18bb8dfd7c1d59742
JungwanPark01 <37926175+JungwanPark01@users.noreply.github.com>
2018-11-29 18:27:51 +0900

    realmain 만들기
    정상적인 위치에 drop 해준다

--- finale/realmain.py
+++ finale/realmain.py
@@ -13,7 +13,7 @@ pygame.init()
 # 돌의 개수
 num_of_stone = 10
 
-stone_particles = [Particle_of_Stone(start_x=(i-(i//5)*5)*150+90,start_y=(i//5)*580+10, team=i//5,surface=surface) for i in range(num_of_stone)] #서피스(게임판)전달
+stone_particles = [Particle_of_Stone(start_x=(i-(i//5)*5)*70+110,start_y=(i//5)*460+15, team=i//5,surface=surface) for i in range(num_of_stone)] #서피스(게임판)전달
 num_particles = num_particles_orig
 particles = [Particle(state=0) for i in range(num_particles)] #중력장을 위한 요소
 
@@ -33,7 +33,7 @@ def new_draw(): #돌 클래스에서 게임판을 전달받았으므로 draw에�
          print(1)
 
     for q in stone_particles:
-        q.draw()
+        q.draw(surface)
 
     pygame.display.flip()
 
